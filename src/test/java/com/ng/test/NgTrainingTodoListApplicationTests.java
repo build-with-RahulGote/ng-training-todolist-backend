7e6221d542025c63b2e742f76ac1f0b3ea909d65
rahulgote4 <goterahul2000@gmail.com>
2025-07-08 23:45:57 +0530

    Add Spring Boot project

---
+++ src/test/java/com/ng/test/NgTrainingTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ng.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgTrainingTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
